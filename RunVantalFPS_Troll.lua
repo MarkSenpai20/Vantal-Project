71f5e916df37c314636b8f58a8590da7998bd53e
MarkSenpai20 <99718398+MarkSenpai20@users.noreply.github.com>
2025-12-19 14:28:30 +0800

    Add script to load Vantal FPS Troll from URL

---
+++ RunVantalFPS_Troll.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MarkSenpai20/Vantal-Project/refs/heads/main/obfuscated_FINAL__Vantal_FPS_TROLL--EDITION---V4-2---(RADIUS%20UPDATE).lua"))()
